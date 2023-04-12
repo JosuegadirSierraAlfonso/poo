01befdb4578915ee0b4cf35c7e9bf9c48790e1f9
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-04-12 13:41:56 -0500

    feat: :arrow_up: Selections

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 class lapiz{
     #marca
     constructor({color = "#702323",
-        dimension="19",
+        dimension="10",
         marca="Mongo",
-        borrador="true",
-        material="Madera"
+        borrador="Tiene",
+        material= "Madera",
     }){
         this.color = color;
         this.dimension = dimension;
@@ -15,10 +15,32 @@ class lapiz{
     getColor(){
         return this.color;
     }
+    getDimension(){
+        return this.dimension;
+    }
+    getMarca(){
+        return this.marca;
+    }
+    getBorrador(){
+        return this.borrador;
+    }
+    getMaterial(){
+        return this.material;
+    }
+
  }
  let obj = undefined;
  let color = document.querySelector(`[name="color"]`)
+ let dimension = document.querySelector(`[name="dimension"]`)
+ let marca = Mongo.checked = true ;
+ let borrador = True.checked = true ;
+ let material = Madera.checked = true ;
+
  addEventListener("DOMContentLoaded", (e)=>{
     obj = new lapiz({});
     color.value = obj.color;
+    dimension.value = obj.dimension;
+    document.getElementById("Mongo").innerHTML = obj.material;
+    document.getElementById("True").innerHTML = obj.material;
+    document.getElementById("Madera").innerHTML = obj.material;
  }) 
